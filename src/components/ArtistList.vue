4ab642df16b13754b25793997207e965abf1870a
typedcode <bug@typedcode.de>
2024-11-01 14:28:48 +0100

    highlight and remember last clicked artist

--- src/components/ArtistList.vue
+++ src/components/ArtistList.vue
@@ -2,14 +2,27 @@
 import { ref } from 'vue';
 import { useMusikcubeStore } from '../stores/musikcube';
 
+const selectedArtistDiv = ref<HTMLDivElement>(undefined);
 const musikcubeStore = useMusikcubeStore();
 
+const selectArtist = (event: PointerEvent, artistId: number) => {
+  if (selectedArtistDiv.value !== undefined) {
+    selectedArtistDiv.value.classList.remove("selected");
+  }
+
+  const target = event.target as HTMLDivElement;
+  target.classList.add("selected");
+
+  selectedArtistDiv.value = target;
+}
 </script>
 
 <template>
   <div class="artistListContainer">
     <ul>
-      <li v-for="artist in musikcubeStore.artists"><a href="">{{ artist.value }}</a></li>
+      <li v-for="artist in musikcubeStore.artists">
+        <div @click="(event) => selectArtist(event, artist.id)">{{ artist.value }}</div>
+      </li>
     </ul>
   </div>
 </template>
@@ -29,7 +42,7 @@ li {
   text-decoration-line: none;
 }
 
-li a {
+li div {
   font: inherit;
   display: block;
   padding: 5px;
@@ -37,7 +50,13 @@ li a {
   color: #c6c6c6
 }
 
-li a:hover {
+li div:hover {
+  color: #373d1f;
+  background-color: #afd700;
+  cursor: pointer;
+}
+
+.selected {
   color: #373d1f;
   background-color: #afd700;
 }
