0407bfa83e38c6f306acc1d3eea267e53e7a87dc
typedcode <bug@typedcode.de>
2024-11-03 00:32:25 +0100

    printing the album name before first track

--- src/components/TrackList.vue
+++ src/components/TrackList.vue
@@ -1,23 +1,41 @@
 <script setup lang="ts">
 
 import { useMusikcubeStore } from '../stores/musikcube';
+import { ref } from 'vue';
 
 const musikcubeStore = useMusikcubeStore();
+
+const headlineNeedsToBePrinted = (index: number) => {
+  if (index === 0) {
+    return true;
+  }
+
+  return musikcubeStore.tracks[index].album !== musikcubeStore.tracks[index - 1].album;
+}
 
 </script>
 
 <template>
   <table>
     <tbody>
-      <tr v-for="track in musikcubeStore.tracks">
-        <td>{{ track.track }}</td>
-        <td>{{ track.title }}</td>
-        <td>len</td>
-        <td>.....</td>
-        <td>{{ track.artist }}</td>
-      </tr>
+      <template v-for="( track, index ) in musikcubeStore.tracks">
+        <tr v-if="headlineNeedsToBePrinted(index)">
+          <th colspan="5">{{ track.album }}</th>
+        </tr>
+        <tr>
+          <td>{{ track.track }}</td>
+          <td>{{ track.title }}</td>
+          <td>len</td>
+          <td>.....</td>
+          <td>{{ track.artist }}</td>
+        </tr>
+      </template>
     </tbody>
   </table>
 </template>
 
-<style scoped></style>
+<style scoped>
+th {
+  color: #afd700;
+}
+</style>
